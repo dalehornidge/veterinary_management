2ab132b1977430052a362d73e6b66a60878685ca
dale hornidge <dalehornidge@gmail.com>
2023-03-28 18:48:58 +0100

    add pet created

--- controllers/pets_controller.py
+++ controllers/pets_controller.py
@@ -25,3 +25,20 @@ def show_pets(id):
 def delete_pet(id):
     pet_repository.delete(id)
     return redirect ("/pets")
+
+@pets_blueprint.route("/pets/new")
+def new_pet():
+    return render_template("/pets/new.html")
+
+@pets_blueprint.route("/pets/new", methods=["POST"])
+def create_pet():
+    name = request.form["name"]
+    dob = request.form["dob"]
+    type = request.form["type"]
+    treatment_notes = request.form["treatment_notes"]
+    owner_id = request.form["owner_id"]
+    vet_id = request.form["vet_id"]
+    new_pet = Pet(name, dob, type, treatment_notes, owner_id, vet_id)
+    pet_repository.save(new_pet)
+    return redirect("/pets")
+
